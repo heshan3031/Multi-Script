c7bfd081ae65de663d963ad29facaeab0d843a38
heshan3031 <111091541+heshan3031@users.noreply.github.com>
2022-09-09 09:10:38 -0500

    Update install-Of.sh

--- R9/install-Of.sh
+++ R9/install-Of.sh
@@ -88,7 +88,7 @@
  }
  
  post_reboot(){
-   echo 'wget -O /root/install.sh "https://raw.githubusercontent.com/rudi9999/ADMRufu/main/install.sh"; clear; sleep 2; chmod +x /root/install.sh; /root/install.sh --continue' >> /root/.bashrc
+   echo 'wget -O /root/install.sh "https://raw.githubusercontent.com/heshan3031/Multi-Script/main/R9/install-Of.sh"; clear; sleep 2; chmod +x /root/install.sh; /root/install.sh --continue' >> /root/.bashrc
    title "INSTALADOR ADMRufu"
    print_center -ama "La instalacion continuara\ndespues del reinicio!!!"
    msg -bar
