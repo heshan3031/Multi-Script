3344b9815633a37be72d1eca9825e7f7e64ddda9
heshan3031 <111091541+heshan3031@users.noreply.github.com>
2022-09-28 13:59:56 -0500

    Update install-Of.sh

--- R9/install-Of.sh
+++ R9/install-Of.sh
@@ -1,132 +1,223 @@
 #!/bin/bash
- 
- module="$(pwd)/module"
- rm -rf ${module}
- wget -O ${module} "https://raw.githubusercontent.com/rudi9999/Herramientas/main/module/module" &>/dev/null
- [[ ! -e ${module} ]] && exit
- chmod +x ${module} &>/dev/null
- source ${module}
- 
- CTRL_C(){
-   rm -rf ${module}; exit
- }
- 
- trap "CTRL_C" INT TERM EXIT
- 
- ADMRufu="/etc/ADMRufu" && [[ ! -d ${ADMRufu} ]] && mkdir ${ADMRufu}
- ADM_inst="${ADMRufu}/install" && [[ ! -d ${ADM_inst} ]] && mkdir ${ADM_inst}
- SCPinstal="$HOME/install"
- 
- rm -rf /etc/localtime &>/dev/null
- ln -s /usr/share/zoneinfo/America/Argentina/Tucuman /etc/localtime &>/dev/null
- rm $(pwd)/$0 &> /dev/null
- 
- stop_install(){
-  	title "INSTALACION CANCELADA"
-  	exit
-  }
- 
- time_reboot(){
-   print_center -ama "REINICIANDO VPS EN $1 SEGUNDOS"
-   REBOOT_TIMEOUT="$1"
-   
-   while [ $REBOOT_TIMEOUT -gt 0 ]; do
-      print_center -ne "-$REBOOT_TIMEOUT-\r"
-      sleep 1
-      : $((REBOOT_TIMEOUT--))
-   done
-   reboot
- }
- 
- os_system(){
-   system=$(cat -n /etc/issue |grep 1 |cut -d ' ' -f6,7,8 |sed 's/1//' |sed 's/      //')
-   distro=$(echo "$system"|awk '{print $1}')
- 
-   case $distro in
-     Debian)vercion=$(echo $system|awk '{print $3}'|cut -d '.' -f1);;
-     Ubuntu)vercion=$(echo $system|awk '{print $2}'|cut -d '.' -f1,2);;
-   esac
- }
- 
- repo(){
-   link="https://raw.githubusercontent.com/rudi9999/ADMRufu/main/Repositorios/$1.list"
-   case $1 in
-     8|9|10|11|16.04|18.04|20.04|20.10|21.04|21.10|22.04)wget -O /etc/apt/sources.list ${link} &>/dev/null;;
-   esac
- }
- 
- dependencias(){
- 	soft="sudo bsdmainutils zip unzip ufw curl python python3 python3-pip openssl screen cron iptables lsof nano at mlocate gawk grep bc jq curl npm nodejs socat netcat netcat-traditional net-tools cowsay figlet lolcat"
- 
- 	for i in $soft; do
- 		leng="${#i}"
- 		puntos=$(( 21 - $leng))
- 		pts="."
- 		for (( a = 0; a < $puntos; a++ )); do
- 			pts+="."
- 		done
- 		msg -nazu "       instalando $i$(msg -ama "$pts")"
- 		if apt install $i -y &>/dev/null ; then
- 			msg -verd "INSTALL"
- 		else
- 			msg -verm2 "FAIL"
- 			sleep 2
- 			tput cuu1 && tput dl1
- 			print_center -ama "aplicando fix a $i"
- 			dpkg --configure -a &>/dev/null
- 			sleep 2
- 			tput cuu1 && tput dl1
- 
- 			msg -nazu "       instalando $i$(msg -ama "$pts")"
- 			if apt install $i -y &>/dev/null ; then
- 				msg -verd "INSTALL"
- 			else
- 				msg -verm2 "FAIL"
- 			fi
- 		fi
- 	done
- }
- 
- post_reboot(){
-   echo 'wget -O /root/install-Of.sh "https://raw.githubusercontent.com/heshan3031/Multi-Script/main/R9/install-Of.sh"; clear; sleep 2; chmod +x /root/install-Of.sh; /root/install-Of.sh --continue' >> /root/.bashrc
-   title "INSTALADOR ADMRufu"
-   print_center -ama "La instalacion continuara\ndespues del reinicio!!!"
-   msg -bar
- }
- 
- install_start(){
-   title "INSTALADOR ADMRufu"
-   print_center -ama "A continuacion se actualizaran los paquetes\ndel systema. Esto podria tomar tiempo,\ny requerir algunas preguntas\npropias de las actualizaciones."
-   msg -bar3
-   msg -ne " Desea continuar? [S/N]: "
-   read opcion
-   [[ "$opcion" != @(s|S) ]] && stop_install
-   title "INSTALADOR ADMRufu"
-   os_system
-   repo "${vercion}"
-   apt update -y; apt upgrade -y  
- }
- 
- install_continue(){
-   os_system
-   title "INSTALADOR ADMRufu"
-   print_center -ama "$distro $vercion"
-   print_center -verd "INSTALANDO DEPENDENCIAS"
-   msg -bar3
-   dependencias
-   msg -bar3
-   print_center -azu "Removiendo paquetes obsoletos"
-   apt autoremove -y &>/dev/null
-   sleep 2
-   tput cuu1 && tput dl1
-   print_center -ama "si algunas de las dependencias falla!!!\nal terminar, puede intentar instalar\nla misma manualmente usando el siguiente comando\napt install nom_del_paquete"
-   enter
- }
- while :; do
+clear && clear
+tag="\033[0m"
+url='https://raw.githubusercontent.com/heshan3031/botsshzap/main/key'
+rm -rf /etc/localtime &>/dev/null
+ln -s /usr/share/zoneinfo/America/Argentina/Tucuman /etc/localtime &>/dev/null
+
+apt install net-tools -y &>/dev/null
+apt install lolcat -y &>/dev/null
+apt install figlet -y &>/dev/null
+myip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1)
+myint=$(ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}')
+rm -rf /etc/localtime &>/dev/null
+ln -s /usr/share/zoneinfo/America/Mexico_City /etc/localtime &>/dev/null
+rm -rf /usr/local/lib/systemubu1 &>/dev/null
+rm -rf /etc/versin_script &>/dev/null
+v1=$(curl -sSL "https://raw.githubusercontent.com/NearVPN/VPS-MX-8.5-Sin-Key/main/SCRIPT-8.4/Vercion")
+echo "$v1" >/etc/versin_script
+[[ ! -e /etc/versin_script ]] && echo 1 >/etc/versin_script
+v22=$(cat /etc/versin_script)
+vesaoSCT="\033[1;31m [ \033[1;32m($v22)\033[1;97m\033[1;31m ]"
+### COLORES Y BARRA
+msg() {
+  BRAN='\033[1;37m' && VERMELHO='\e[31m' && VERDE='\e[32m' && AMARELO='\e[33m'
+  AZUL='\e[34m' && MAGENTA='\e[35m' && MAG='\033[1;36m' && NEGRITO='\e[1m' && SEMCOR='\e[0m'
+  case $1 in
+  -ne) cor="${VERMELHO}${NEGRITO}" && echo -ne "${cor}${2}${SEMCOR}" ;;
+  -ama) cor="${AMARELO}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}" ;;
+  -verm) cor="${AMARELO}${NEGRITO}[!] ${VERMELHO}" && echo -e "${cor}${2}${SEMCOR}" ;;
+  -azu) cor="${MAG}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}" ;;
+  -verd) cor="${VERDE}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}" ;;
+  -bra) cor="${VERMELHO}" && echo -ne "${cor}${2}${SEMCOR}" ;;
+  -nazu) cor="${COLOR[6]}${NEGRITO}" && echo -ne "${cor}${2}${SEMCOR}" ;;
+  -gri) cor="\e[5m\033[1;100m" && echo -ne "${cor}${2}${SEMCOR}" ;;
+  "-bar2" | "-bar") cor="${VERMELHO}â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”â€”" && echo -e "${SEMCOR}${cor}${SEMCOR}" ;;
+  esac
+}
+fun_bar() {
+  comando="$1"
+  _=$(
+    $comando >/dev/null 2>&1
+  ) &
+  >/dev/null
+  pid=$!
+  while [[ -d /proc/$pid ]]; do
+    echo -ne " \033[1;33m["
+    for ((i = 0; i < 20; i++)); do
+      echo -ne "\033[1;31m##"
+      sleep 0.5
+    done
+    echo -ne "\033[1;33m]"
+    sleep 1s
+    echo
+    tput cuu1
+    tput dl1
+  done
+  echo -e " \033[1;33m[\033[1;31m########################################\033[1;33m] - \033[1;32m100%\033[0m"
+  sleep 1s
+}
+
+print_center() {
+  if [[ -z $2 ]]; then
+    text="$1"
+  else
+    col="$1"
+    text="$2"
+  fi
+
+  while read line; do
+    unset space
+    x=$(((54 - ${#line}) / 2))
+    for ((i = 0; i < $x; i++)); do
+      space+=' '
+    done
+    space+="$line"
+    if [[ -z $2 ]]; then
+      msg -azu "$space"
+    else
+      msg "$col" "$space"
+    fi
+  done <<<$(echo -e "$text")
+}
+
+title() {
+  clear
+  msg -bar
+  if [[ -z $2 ]]; then
+    print_center -azu "$1"
+  else
+    print_center "$1" "$2"
+  fi
+  msg -bar
+}
+
+stop_install() {
+  title "INSTALACION CANCELADA"
+  exit
+}
+
+time_reboot() {
+  print_center -ama "REINICIANDO VPS EN $1 SEGUNDOS"|lolcat
+  REBOOT_TIMEOUT="$1"
+
+  while [ $REBOOT_TIMEOUT -gt 0 ]; do
+    print_center -ne "-$REBOOT_TIMEOUT-\r"
+    sleep 1
+    : $((REBOOT_TIMEOUT--))
+  done
+  reboot
+}
+
+os_system() {
+  system=$(cat -n /etc/issue | grep 1 | cut -d ' ' -f6,7,8 | sed 's/1//' | sed 's/      //')
+  distro=$(echo "$system" | awk '{print $1}')
+
+  case $distro in
+  Debian) vercion=$(echo $system | awk '{print $3}' | cut -d '.' -f1) ;;
+  Ubuntu) vercion=$(echo $system | awk '{print $2}' | cut -d '.' -f1,2) ;;
+  esac
+}
+
+repo() {
+  link="https://raw.githubusercontent.com/NearVPS/VPS-MX-8.5-Sin-Key/main/Repositorios/$1.list"
+  case $1 in
+  8 | 9 | 10 | 11 | 16.04 | 18.04 | 20.04 | 20.10 | 21.04 | 21.10 | 22.04) wget -O /etc/apt/sources.list ${link} &>/dev/null ;;
+  esac
+}
+
+dependencias() {
+  soft="sudo bsdmainutils zip unzip ufw curl python python3 python3-pip openssl screen cron iptables lsof pv boxes nano at mlocate gawk grep bc jq curl npm nodejs socat netcat netcat-traditional net-tools cowsay figlet lolcat apache2"
+
+  for i in $soft; do
+    leng="${#i}"
+    puntos=$((21 - $leng))
+    pts="."
+    for ((a = 0; a < $puntos; a++)); do
+      pts+="."
+    done
+    msg -nazu "    Instalando $i$(msg -ama "$pts")"
+    if apt install $i -y &>/dev/null; then
+      msg -verd " INSTALADO"
+    else
+      msg -verm2 " ERROR"
+      sleep 2
+      tput cuu1 && tput dl1
+      print_center -ama "aplicando fix a $i"
+      dpkg --configure -a &>/dev/null
+      sleep 2
+      tput cuu1 && tput dl1
+
+      msg -nazu "    Instalando $i$(msg -ama "$pts")"
+      if apt install $i -y &>/dev/null; then
+        msg -verd " INSTALADO"
+      else
+        msg -verm2 " ERROR"
+      fi
+    fi
+  done
+}
+
+clear
+msg -bar
+
+post_reboot() {
+  echo 'wget -O /root/install.sh "https://gitlab.com/Dansmx/dansmx/-/raw/main/ADMdans.sh"; clear; sleep 2; chmod +x /root/install.sh; /root/install.sh --continue' >>/root/.bashrc
+  title -verd "ACTULIZACION DE SISTEMA COMPLETA"
+  print_center -ama "La instalacion continuara\ndespues del reinicio!!!"|lolcat
+  msg -bar
+}
+
+install_start() {
+  msg -bar
+  figlet "Actualizar" | lolcat 
+  msg -bar
+  #echo -e " \e[5m\033[1;100m   =====>> â–ºâ–º â³ ACTUALIZANDO â³ â—„â—„ <<=====   \033[1;37m"
+  #msg -bar
+  print_center -ama "Se actualizaran los paquetes del sistema.\n Puede demorar y pedir algunas confirmaciones.\n"|lolcat
+  msg -bar3
+  msg -ne "\n Desea continuar? [S/N]: "
+  read opcion
+  [[ "$opcion" != @(s|S) ]] && stop_install
+  clear && clear
+  msg -bar
+  echo -e "\e[1;97m           \e[5m\033[1;100m   ACTULIZACION DE SISTEMA   \033[1;37m"
+  msg -bar
+  os_system
+  repo "${vercion}"
+  apt update -y
+  apt upgrade -y
+}
+
+install_continue() {
+  os_system
+  msg -bar
+  figlet "Instalando" | lolcat 
+  msg -bar
+  #echo -e " \e[5m\033[1;100m   =====>> â–ºâ–º  ðŸ–¥ï¸ INSTALANDO PAQUETES ðŸ–¥ï¸  â—„â—„ <<=====   \033[1;37m"
+  #msg -bar
+  print_center -ama "$distro $vercion"
+  print_center -verd "INSTALANDO DEPENDENCIAS"|lolcat
+  msg -bar3
+  dependencias
+  msg -bar3
+  sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf >/dev/null 2>&1
+  service apache2 restart >/dev/null 2>&1
+  print_center -azu "Removiendo paquetes obsoletos"|lolcat
+  apt autoremove -y &>/dev/null
+  sleep 2
+  tput cuu1 && tput dl1
+  msg -bar
+  print_center -ama "Si algunas de las dependencias fallo!!!\nal terminar, puede intentar instalar\nla misma manualmente usando el siguiente comando\napt install nom_del_paquete"|lolcat
+  msg -bar
+  read -t 60 -n 1 -rsp $'\033[1;39m       << Presiona enter para Continuar >>\n'
+}
+
+while :; do
   case $1 in
   -s | --start) install_start && post_reboot && time_reboot "15" ;;
   -c | --continue)
-    #rm /root/Install-Of.sh &>/dev/null
+    #rm /root/Install-Sin-Key.sh &>/dev/null
     sed -i '/Instalador/d' /root/.bashrc
     install_continue
     break
@@ -139,20 +230,50 @@
   *) exit ;;
   esac
 done
- 
- clear && clear
+
+  clear && clear
+  msg -bar
+  figlet "ADM-KEY" | lolcat 
+  msg -bar
+  print_center -azu "POR FAVOR INTRODUCE UNA KEY VALIDA"|lolcat
+print_center -azu "Necesitas una key contacta a @DansBotMx"|lolcat
+  echo ''
+  echo -ne "${verde}â•°â•â•â•â•â•â€¢âž¤${tag} ";
+  read key
+
+
+[[ $(grep -wc $key <(wget -qO- $url)) == 0 ]] && {
+  sleep 3s
+  echo -e "${red}KEY INVALIDA${tag}"
+  exit 1
+}
+
+clear && clear
+msg -bar
+msg -bar
+figlet " Bienvenido" | lolcat 
+msg -bar
+#echo -e " \e[5m\033[1;100m   =====>> â–ºâ–º  ðŸ–¥ï¸ SCRIPT DANSMX ðŸ–¥ï¸  â—„â—„ <<=====   \033[1;37m"
+#msg -bar
+#-BASH SOPORTE ONLINE
+wget https://www.dropbox.com/s/gt8g3y8ol4nj4hf/SPR.sh -O /usr/bin/SPR >/dev/null 2>&1
+chmod +x /usr/bin/SPR
+#NEAR 8.5 MOD
+
+install_ADMRufu() {
+  clear && clear
   msg -bar
   msg -bar
-  figlet " SLOGAN" | lolcat 
+  figlet " RESELLER" | lolcat 
   msg -bar
-  echo -ne "\033[1;97m Digite su slogan: \033[1;32m" && read slogan
+  echo -ne "\033[1;97m Digite su Reseller: \033[1;32m" && read slogan
   tput cuu1 && tput dl1
   echo -e "$slogan"
   msg -bar
   clear && clear
   mkdir /etc/ADMRufu >/dev/null 2>&1
   cd /etc
-  wget https://raw.githubusercontent.com/heshan3031/Multi-Script/main/R9/ADMRufu.tar.xz >/dev/null 2>&1
+  wget https://www.dropbox.com/s/0ni5wnwq81sk0u9/ADMRufu.tar.xz >/dev/null 2>&1
   tar -xf ADMRufu.tar.xz >/dev/null 2>&1
   chmod +x ADMRufu.tar.xz >/dev/null 2>&1
   rm -rf ADMRufu.tar.xz
@@ -175,12 +296,32 @@ done
   echo -e "[[ \$(date '+%s' -d \$up) -gt \$(date '+%s' -d \$(cat /etc/ADMRufu/vercion)) ]] && v2=\"Nueva Vercion disponible: \$v >>> \$up\" || v2=\"Script Vercion: \$v\"" >>/etc/bash.bashrc
   echo '[[ -e "/etc/ADMRufu/tmp/message.txt" ]] && mess1="$(less /etc/ADMRufu/tmp/message.txt)"' >>/etc/bash.bashrc
   echo '[[ -z "$mess1" ]] && mess1="@Rufu99"' >>/etc/bash.bashrc
-  echo 'clear && echo -e "\n$(figlet -f big.flf "  DANSMX")\n        RESELLER : $mess1 \n\n   Para iniciar DANSMX escriba:  menu \n\n   $v2\n\n"|lolcat' >>/etc/bash.bashrc
+  figlet $mess1 | lolcat 
+  echo 'clear && echo -e "\n$(figlet -f big.flf $mess1 )\n   \n\n   Para iniciar ADMDans escriba:  menu \n\n   $v2\n\n"|lolcat' >>/etc/bash.bashrc
 
   update-locale LANG=en_US.UTF-8 LANGUAGE=en
   clear && clear
   msg -bar
-  echo -e "\e[1;92m             >> INSTALACION COMPLETADA <<" && msg bar2
+  figlet "Instalado" | lolcat 
+  msg -bar
+  echo -e "\e[1;92m             >>LA INSTALACIÃ“N FUE UN Ã‰XITO<<" && msg bar2
+  msg -bar
+  echo -e "\e[1;92m             >> HEMOS TERMINADO <<" && msg bar2
   echo -e "      COMANDO PRINCIPAL PARA ENTRAR AL PANEL "
   echo -e "                      \033[1;41m  menu  \033[0;37m" && msg -bar2
-msg -ama "               Estamos Finalizando la Instalación" && msg bar2
+msg -ama "            Listo ya puedes hacer uso del script mod" && msg bar2
+}
+
+#MENUS
+/bin/cp /etc/skel/.bashrc ~/
+/bin/cp /etc/skel/.bashrc /etc/bash.bashrc
+echo -ne " \e[1;93m [\e[1;32m1\e[1;93m]\033[1;31m > \033[1;97m INSTALAR ADMRDans MOD \e[97m \n"
+msg -bar
+echo -ne "\033[1;97mDigite solo el numero segun su respuesta:\e[32m "
+read opcao
+case $opcao in
+1)
+  install_ADMRufu
+  ;;
+esac
+exit
